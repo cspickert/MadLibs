44d912826cc91cececc1ca6ecaa5f7a409aa7b45
Cameron Spickert <cspickert@users.noreply.github.com>
2015-06-07 16:30:23 -0400

    Initial Commit

---
+++ MadLibs/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MadLibs
+//
+//  Created by Cameron Spickert on 6/7/15.
+//  Copyright (c) 2015 Cameron Spickert. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
